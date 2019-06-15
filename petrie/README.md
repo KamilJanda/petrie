c3a24d6d2eb09d328f0dbecb03279de543d6f819
Jakub <jsroka@virtuslab.com>
2019-06-15 17:42:57 +0200

    simple links request bugfix plus client name changed from xxx to xxxx

--- petrie/README.md
+++ petrie/README.md
@@ -7,7 +7,7 @@ install postgres on your local machine.
 than create database and user with this commands: 
 
 ```sql
-CREATE ROLE xxx WITH LOGIN PASSWORD 'pass123';
+CREATE ROLE xxxx WITH LOGIN PASSWORD 'pass123';
 CREATE DATABASE petrie;
-GRANT ALL PRIVILEGES ON DATABASE petrie TO xxx;
+GRANT ALL PRIVILEGES ON DATABASE petrie TO xxxx;
 ```
--- petrie/modules/scraping/src/main/scala/agh/petrie/scraping/actors/Getter.scala
+++ petrie/modules/scraping/src/main/scala/agh/petrie/scraping/actors/Getter.scala
@@ -1,11 +1,14 @@
 package agh.petrie.scraping.actors
 
-import akka.actor.{Actor, Props}
-import akka.pattern.pipe
 import agh.petrie.scraping.actors.Controller.CheckUrl
+import agh.petrie.scraping.actors.Getter.Stop
 import agh.petrie.scraping.service.HtmlParsingService
 import agh.petrie.scraping.web.AsyncScrapingService
 import agh.petrie.scraping.web.AsyncScrapingService.Html
+import akka.actor.{Actor, Props}
+import akka.pattern.pipe
+
+import scala.concurrent.duration._
 
 class Getter(
   url:                  String,
@@ -17,12 +20,16 @@ class Getter(
   implicit val exec = context.dispatcher
 
   asyncScrapingService.getUrlContent(url) pipeTo self
+  context.system.scheduler.scheduleOnce(2 second, self, Stop)
 
   override def receive: Receive = {
     case html: Html =>
       htmlParsingService.fetchUrls(html).foreach{ url =>
         context.parent ! CheckUrl(url, depth - 1)
       }
+      context.parent ! Controller.CheckDone
+      context.stop(self)
+    case Stop =>
       context.parent ! Controller.CheckDone
       context.stop(self)
   }
@@ -36,4 +43,5 @@ object Getter {
     htmlParsingService: HtmlParsingService
   ) = Props(new Getter(url, depth, asyncScrapingService, htmlParsingService))
 
+  case object Stop
 }
