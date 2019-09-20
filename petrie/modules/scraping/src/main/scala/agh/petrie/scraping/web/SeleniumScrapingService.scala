c835db29b029260dfe54289e79f1d2f4893c073c
Jakub <jsroka@virtuslab.com>
2019-09-20 14:23:30 +0200

    PPPETRIE-6

--- petrie/modules/scraping/src/main/scala/agh/petrie/scraping/web/SeleniumScrapingService.scala
+++ petrie/modules/scraping/src/main/scala/agh/petrie/scraping/web/SeleniumScrapingService.scala
@@ -1,10 +1,13 @@
 package agh.petrie.scraping.web
 
-import agh.petrie.scraping.model.Configuration
+import agh.petrie.scraping.model.{Configuration, CssSelector, PreScrapingConfiguration, XpathSelector}
 import agh.petrie.scraping.service.HtmlParsingService.Html
 import com.softwaremill.macwire.wire
-import org.openqa.selenium.WebDriver
+import org.openqa.selenium.{By, Keys, WebDriver}
 import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
+import org.openqa.selenium.By
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -13,9 +16,29 @@ class SeleniumScrapingService {
   private[scraping] lazy val options = wire[ChromeOptions]
   private[scraping] lazy val driver: WebDriver = wire[ChromeDriver]
 
-  def getUrlContent(url: String, configuration: Configuration)(implicit ec: ExecutionContext) = {
+  def getUrlContent(
+    url: String,
+    preScrapingConfiguration: Option[PreScrapingConfiguration]
+  )(implicit ec: ExecutionContext) = {
     Future {
       driver.get(url)
+      preScrapingConfiguration.foreach(config =>
+        config.elementsToClick.foreach( element =>
+          if (element.selectorType == CssSelector) {
+            val wait = new WebDriverWait(driver, 10)
+            try {
+              wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(element.selector)))
+              val el = driver.findElement(By.cssSelector(element.selector))
+              //println("clicking element of size: " + el.getSize)
+              el.click()
+            } catch {
+              case e => println(e)
+            }
+          } else {
+            driver.findElement(By.xpath(element.selector)).click()
+          }
+        )
+      )
       Html(driver.getPageSource)
     }
   }
